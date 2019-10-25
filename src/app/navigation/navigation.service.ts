109cfd7fdacfb5a4cd378ef1a601cc9ac786b312
petr <stechy12@gmail.com>
2019-10-25 17:27:28 +0200

    Upravení velikosti navigační lišty.

--- src/app/navigation/navigation.service.ts
+++ src/app/navigation/navigation.service.ts
@@ -3,16 +3,30 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter, map, mergeMap } from 'rxjs/operators';
 
+/**
+ * Služba starající se o navigační a postraní lištu
+ * Disponuje metodami pro přepínání viditelnosti postraní lišty
+ * a obsahuje vlastnosti pro nastavení nadpisu a podnadpisu stránky
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
+  private _showSidebar: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
   public title: string;
+  public subtitle: string;
+  public working: boolean;
 
   constructor(private readonly _route: ActivatedRoute,
               private readonly _router: Router) {
 
+    /**
+     * Přihlásí se k odběru událostí na změnu url
+     * Pokud daná routa obsahuje v datech parametr 'title',
+     * dosadí ho jako titulek
+     */
     this._router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(() => this._route),
@@ -27,17 +41,25 @@ export class NavigationService {
         .subscribe((event) => this.title = event['title']);
   }
 
-  private _showSidebar: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  /**
+   * Přepne viditelnost postraní lišty
+   */
+  toggle() {
+    this._showSidebar.next(!this._showSidebar.getValue());
+  }
 
+  /**
+   * Nastaví viditelnost postraní lišty
+   * @param show True, pokud se má lišta zobrazit, jinak false
+   */
   set showSidebar(show: boolean) {
     this._showSidebar.next(show);
   }
 
+  /**
+   * Vrátí pozorovatelnou hodnotu pro viditelnost postraní lišty
+   */
   get showSidebarValue(): Observable<boolean> {
     return this._showSidebar;
-  }
-
-  toggle() {
-    this._showSidebar.next(!this._showSidebar.getValue());
   }
 }
