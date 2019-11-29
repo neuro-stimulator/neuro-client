3874b028e81ad82140e0d04b99ba0a7e4230c3aa
petr <stechy12@gmail.com>
2019-11-29 15:09:42 +0100

    Implementace FVEP konfigurace.

--- src/app/experiments/experiment-type/experiment-type-fvep/experiment-type-fvep.component.ts
+++ src/app/experiments/experiment-type/experiment-type-fvep/experiment-type-fvep.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AbstractControl, FormControl, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 
 import { ToastrService } from 'ngx-toastr';
-import { createEmptyExperiment, Experiment } from 'diplomka-share';
+import { Options as SliderOptions } from 'ng5-slider/options';
+
+import { environment } from '../../../../environments/environment';
+
+import { ExperimentType, ExperimentFVEP } from 'diplomka-share';
 
 import { ExperimentsService } from '../../experiments.service';
 import { BaseExperimentTypeComponent } from '../base-experiment-type.component';
@@ -13,7 +18,26 @@ import { BaseExperimentTypeComponent } from '../base-experiment-type.component';
   templateUrl: './experiment-type-fvep.component.html',
   styleUrls: ['./experiment-type-fvep.component.sass']
 })
-export class ExperimentTypeFvepComponent extends BaseExperimentTypeComponent<Experiment> implements OnInit {
+export class ExperimentTypeFvepComponent extends BaseExperimentTypeComponent<ExperimentFVEP> implements OnInit {
+
+  outputCountParams: SliderOptions = {
+    floor: 1,
+    ceil: environment.maxOutputCount,
+    showTicks: true,
+    showTicksValues: true,
+    tickStep: 1,
+    animate: false
+  };
+
+  brightnessSliderOptions: SliderOptions = {
+    floor: 0,
+    ceil: 100,
+    showTicks: false,
+    showTicksValues: false,
+    tickStep: 1,
+    showSelectionBar: true,
+    animate: false
+  };
 
   constructor(service: ExperimentsService,
               toastr: ToastrService,
@@ -28,8 +52,58 @@ export class ExperimentTypeFvepComponent extends BaseExperimentTypeComponent<Exp
     super.ngOnInit();
   }
 
-  protected _createEmptyExperiment(): Experiment {
-    return createEmptyExperiment();
+  protected _createFormControls(): { [p: string]: AbstractControl } {
+    const superControls = super._createFormControls();
+    const myControls = {
+      outputCount: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(environment.maxOutputCount)]),
+      timeOn: new FormControl(null, [Validators.required]),
+      timeOff: new FormControl(null, [Validators.required]),
+      timeFrequency: new FormControl(null, [Validators.required]),
+      timeDutyCycle: new FormControl(null, [Validators.required]),
+      brightness: new FormControl(null, [Validators.required])
+    };
+
+    return {...superControls, ...myControls};
   }
 
+  protected _createEmptyExperiment(): ExperimentFVEP {
+    return {
+      name: '',
+      description: '',
+      created: new Date().getTime(),
+      type: ExperimentType.FVEP,
+      output: {},
+      outputCount: 1,
+      timeOn: 0,
+      timeOff: 0,
+      frequency: 0,
+      dutyCycle: 0,
+      brightness: 0
+    };
+  }
+
+
+  get outputCount() {
+    return this.form.get('outputCount');
+  }
+
+  get timeOn() {
+    return this.form.get('timeOn');
+  }
+
+  get timeOff() {
+    return this.form.get('timeOff');
+  }
+
+  get frequency() {
+    return this.form.get('frequency');
+  }
+
+  get dutyCycle() {
+    return this.form.get('dutyCycle');
+  }
+
+  get brightness() {
+    return this.form.get('brightness');
+  }
 }
