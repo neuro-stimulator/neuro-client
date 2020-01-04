a3b59829611e32a203dc7351a065059dbb6e4bcf
petr <stechy12@gmail.com>
2020-01-04 14:02:43 +0100

    Upravení proměnných pro produkční verzi.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,19 @@
+import { SERVER_HTTP_PORT, SERVER_SOCKET_PORT } from 'diplomka-share';
+
 export const environment = {
-  production: true,
+  production: false,
   url: {
-    server: 'http://localhost',
-    socket: 'http://localhost',
+    server: '',
+    socket: '',
   },
   port: {
-    server: 3000,
-    socket: 3001
+    server: SERVER_HTTP_PORT,
+    socket: SERVER_SOCKET_PORT
   },
   makeURL: (url: string, port: number) => {
-    return `${url}:${port}`;
-  }
+    // return `${url}:${port}`;
+    return '';
+  },
+  maxOutputCount: 8,
+  patternSize: 32
 };
