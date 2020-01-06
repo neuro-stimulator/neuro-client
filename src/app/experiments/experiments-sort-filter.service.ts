f78d26aee1415836c99c2031066bebaa258b422a
petr <stechy12@gmail.com>
2020-01-06 21:03:37 +0100

    Použití knihovny FuseJS pro vyhledávání v experimentech.

--- src/app/experiments/experiments-sort-filter.service.ts
+++ src/app/experiments/experiments-sort-filter.service.ts
@@ -1,24 +1,51 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { ExperimentsService } from './experiments.service';
+
+import * as Fuse from 'fuse.js';
+
 import { Experiment } from '@stechy1/diplomka-share';
+
+import { ExperimentsService } from './experiments.service';
+import { NGXLogger } from 'ngx-logger';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperimentsSortFilter {
 
+  private static readonly FUSEJS_SETTINGS = {
+    tokenize: true,
+    includeScore: true,
+    includeMatches: true,
+    threshold: 0.4,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: ['name']
+  };
+
+  // Fusejs instance pro fulltextové vyhledávání
+  private readonly _fusejs: Fuse<Experiment, Fuse.FuseOptions<Experiment>>;
   // Způsob seskupování
   private _selectedGroup: string;
   // Způsob řazení
   private _orderBy$: EventEmitter<string> = new EventEmitter<string>();
-  // Kolekce Experimentů
-  private _fuseQueries: Experiment[] = [];
+  // Kolekce experimentů pro FuseJS
+  private _fuseExperiments: Experiment[] = [];
+  // Výsledná kolekce, která obsahuje výsledky experimentů po filtraci
+  private _resultExperiments: Experiment[] = [];
+  // Poslední slovo, podle kterého se vyhledávalo
+  private _lastSearch = '';
 
-  constructor(private readonly service: ExperimentsService) {
+  constructor(private readonly service: ExperimentsService,
+              private readonly logger: NGXLogger) {
     this._selectedGroup = 'none';
+    this._fusejs = new Fuse(this._fuseExperiments, ExperimentsSortFilter.FUSEJS_SETTINGS);
     this.service.records.subscribe(value => {
       // Provedu mělkou kopii pole
-      this._fuseQueries = [...value];
+      this._fuseExperiments.splice(0);
+      this._fuseExperiments.push(...value);
+      this.filterBy(this._lastSearch);
     });
   }
 
@@ -29,14 +56,14 @@ export class ExperimentsSortFilter {
    * @param orderType Směr řazení (vzestupně/sestupně)
    */
   public sort(orderBy: string, orderType: string) {
-    this._fuseQueries.sort((a, b) => a.name.localeCompare(b.name));
+    this._resultExperiments.sort((a, b) => a.name.localeCompare(b.name));
     switch (orderBy) {
       case 'date_of_creation':
-        this._fuseQueries.sort((a, b) => a.created - b.created);
+        this._resultExperiments.sort((a, b) => a.created - b.created);
         break;
     }
     if (orderType === 'descending') {
-      this._fuseQueries.reverse();
+      this._resultExperiments.reverse();
     }
     this._orderBy$.next(orderBy);
   }
@@ -47,23 +74,29 @@ export class ExperimentsSortFilter {
    * @param searchedValue Fulltextová hodnota
    */
   public filterBy(searchedValue: string) {
+    this.logger.debug(`Filtruji data podle: ${searchedValue}.`);
+    this._lastSearch = searchedValue;
+    if (searchedValue === '') {
+      // Ve vyhledávání je prázdná hodnota, nebudu nic filtrovat a zobrazím všechny záznamy
+      this._resultExperiments.splice(0);
+      this._resultExperiments.push(...this._fuseExperiments);
+      return;
+    }
     // Hodnota není prázdná, jdu najít všechny odpovídající dotazy
     // @ts-ignore
-    const fuseResult = []; // this._fusejs.search(searchedValue) as FuseResult<Experiment>[];
-    console.log(fuseResult);
+    const fuseResult = this._fusejs.search(searchedValue) as Fuse.FuseResult<Experiment>[];
     // Vymažu všechny záznamy v pracovní kolekci
-    this._fuseQueries.splice(0);
+    this._resultExperiments.splice(0);
     // Přidám všechny nalezené záznamy do pracovní kolekce
     for (const result of fuseResult) {
       if (result.score < 0.4) {
-        this._fuseQueries.push(result.item);
+        this._resultExperiments.push(result.item);
       }
     }
   }
 
-
   public get records(): Experiment[] {
-    return this._fuseQueries;
+    return this._resultExperiments;
   }
 }
 
