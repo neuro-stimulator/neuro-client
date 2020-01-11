c54e0d74eb7e689fca31a5d6d7e84617197b6b52
petr <stechy12@gmail.com>
2020-01-11 15:53:14 +0100

    Přidání výchozího nastavení i do produkční konfigurace.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,26 @@
 import { SERVER_HTTP_PORT, SERVER_SOCKET_PORT } from '@stechy1/diplomka-share';
+import { Settings } from '../app/settings/settings';
+
+const DEFAULT_SETTINGS: Settings = {
+  experiments: {
+    showDescription: true,
+    showTags: true,
+    showCreationDate: true,
+    showOutputType: true,
+    showOutputCount: true,
+    creationDateFormat: {
+      showYears: true,
+      showMonths: true,
+      showDays: true,
+      showHours: false,
+      showMinutes: false,
+      showSeconds: false,
+      showMiliseconds: false,
+    }
+  },
+  player: {},
+  results: {},
+};
 
 export const environment = {
   production: false,
@@ -11,7 +33,8 @@ export const environment = {
     socket: SERVER_SOCKET_PORT
   },
   maxOutputCount: 8,
-  patternSize: 32
+  patternSize: 32,
+  settings: DEFAULT_SETTINGS
 };
 
 export function makeURL(url: string, port: number) {
