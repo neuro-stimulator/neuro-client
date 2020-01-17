d6094e392c5f38d625592f932373a461864ac23f
petr <stechy12@gmail.com>
2020-01-17 13:11:49 +0100

    Úklid kódu.

--- src/app/experiments/experiments-filter-parameters.ts
+++ src/app/experiments/experiments-filter-parameters.ts
@@ -22,18 +22,13 @@ export class GroupByPosibilities {
   static readonly TAG = new GroupByPosibilities('Tagu', 'TAG', 'fa-tags',
     experiment => experiment.tags,
     (experiment: Experiment, group: string) => experiment.tags.includes(group),
-    (experiment, group) => experiment.tags.length === 0,
+    (experiment: Experiment, group: string) => experiment.tags.length === 0,
     group => group);
-  // static readonly OUTPUT_TYPE = new GroupByPosibilities('Typ výstupu', 'output_type', '',
-  //   experiment => `${experiment.usedOutputs}`,
-  //   (experiment: Experiment, group: OutputType) => (outputTypeToRaw(experiment.usedOutputs) | outputTypeToRaw(group)) !== 0,
-  //   (group: OutputType) => `${outputTypeToRaw(group)}`);
 
   static readonly VALUES: GroupByPosibilities[] = [
     GroupByPosibilities.NONE,
     GroupByPosibilities.TYPE,
     GroupByPosibilities.TAG
-    // GroupByPosibilities.OUTPUT_TYPE
   ];
 
   private constructor(public readonly name: string, public readonly value: string, public readonly icon: string,
@@ -48,15 +43,15 @@ export class SortByPosibilities {
   static readonly KEY = 'sortBy';
 
   static readonly ALPHABET = new SortByPosibilities('Abecedně', 'ALPHABET', {ascending: 'fa-sort-alpha-up', descending: 'fa-sort-alpha-down'},
-    (a, b) => a.name.localeCompare(b.name));
+    (a: Experiment, b: Experiment) => a.name.localeCompare(b.name));
   static readonly CREATION_DATE = new SortByPosibilities('Datum vytvoření', 'CREATION_DATE', {ascending: 'fa-sort-numeric-up', descending: 'fa-sort-numeric-down'},
-    (a, b) => a.created - b.created);
+    (a: Experiment, b: Experiment) => a.created - b.created);
   static readonly TYPE = new SortByPosibilities('Typ experimentu', 'TYPE', {ascending: 'fa-stethoscope', descending: 'fa-stethoscope'},
-    (a, b) => a.type - b.type);
+    (a: Experiment, b: Experiment) => a.type - b.type);
   static readonly OUTPUT_TYPE = new SortByPosibilities('Typ výstupu', 'OUTPUT_TYPE', {ascending: 'fa-plug', descending: 'fa-plug'},
-    (a, b) => outputTypeToRaw(a.usedOutputs) - outputTypeToRaw(b.usedOutputs));
+    (a: Experiment, b: Experiment) => outputTypeToRaw(a.usedOutputs) - outputTypeToRaw(b.usedOutputs));
   static readonly OUTPUT_COUNT = new SortByPosibilities('Počtu výstupu', 'OUTPUT_COUNT', {ascending: 'fa-hashtag', descending: 'fa-plug'},
-    (a, b) => a.outputCount - b.outputCount);
+    (a: Experiment, b: Experiment) => a.outputCount - b.outputCount);
 
   static readonly VALUES: SortByPosibilities[] = [
     SortByPosibilities.ALPHABET,
