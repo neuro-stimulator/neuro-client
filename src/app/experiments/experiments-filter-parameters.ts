bc91b8ee24f54f08596aaa0fbb3de2e988f7d206
petr <stechy12@gmail.com>
2020-01-19 15:50:25 +0100

    Přeložení dialogu pro filtrování experimentů.

--- src/app/experiments/experiments-filter-parameters.ts
+++ src/app/experiments/experiments-filter-parameters.ts
@@ -9,17 +9,17 @@ export interface FilterParameters {
 export class GroupByPosibilities {
   static readonly KEY = 'groupBy';
 
-  static readonly NONE = new GroupByPosibilities('Neseskupovat', 'NONE', 'fa-ban',
+  static readonly NONE = new GroupByPosibilities('EXPERIMENTS.FILTER_DIALOG.GROUP_BY.NONE', 'NONE', 'fa-ban',
     () => '',
     () => true,
     () => false,
     (group => group));
-  static readonly TYPE = new GroupByPosibilities('Typ experimentu', 'TYPE', 'fa-stethoscope',
+  static readonly TYPE = new GroupByPosibilities('EXPERIMENTS.FILTER_DIALOG.GROUP_BY.TYPE', 'TYPE', 'fa-stethoscope',
     experiment => `${experiment.type}`,
     (experiment: Experiment, group: string) => experiment.type === +group,
     () => false,
     (group: string) => ExperimentType[+group]);
-  static readonly TAG = new GroupByPosibilities('Tagu', 'TAG', 'fa-tags',
+  static readonly TAG = new GroupByPosibilities('EXPERIMENTS.FILTER_DIALOG.GROUP_BY.TAG', 'TAG', 'fa-tags',
     experiment => experiment.tags,
     (experiment: Experiment, group: string) => experiment.tags.includes(group),
     (experiment: Experiment, group: string) => experiment.tags.length === 0,
@@ -42,15 +42,20 @@ export class GroupByPosibilities {
 export class SortByPosibilities {
   static readonly KEY = 'sortBy';
 
-  static readonly ALPHABET = new SortByPosibilities('Abecedně', 'ALPHABET', {ascending: 'fa-sort-alpha-up', descending: 'fa-sort-alpha-down'},
+  static readonly ALPHABET = new SortByPosibilities('EXPERIMENTS.FILTER_DIALOG.SORT_BY.ALPHABET', 'ALPHABET',
+    {ascending: 'fa-sort-alpha-up', descending: 'fa-sort-alpha-down'},
     (a: Experiment, b: Experiment) => a.name.localeCompare(b.name));
-  static readonly CREATION_DATE = new SortByPosibilities('Datum vytvoření', 'CREATION_DATE', {ascending: 'fa-sort-numeric-up', descending: 'fa-sort-numeric-down'},
+  static readonly CREATION_DATE = new SortByPosibilities('EXPERIMENTS.FILTER_DIALOG.SORT_BY.CREATION_DATE', 'CREATION_DATE',
+    {ascending: 'fa-sort-numeric-up', descending: 'fa-sort-numeric-down'},
     (a: Experiment, b: Experiment) => a.created - b.created);
-  static readonly TYPE = new SortByPosibilities('Typ experimentu', 'TYPE', {ascending: 'fa-stethoscope', descending: 'fa-stethoscope'},
+  static readonly TYPE = new SortByPosibilities('EXPERIMENTS.FILTER_DIALOG.SORT_BY.TYPE', 'TYPE',
+    {ascending: 'fa-stethoscope', descending: 'fa-stethoscope'},
     (a: Experiment, b: Experiment) => a.type - b.type);
-  static readonly OUTPUT_TYPE = new SortByPosibilities('Typ výstupu', 'OUTPUT_TYPE', {ascending: 'fa-plug', descending: 'fa-plug'},
+  static readonly OUTPUT_TYPE = new SortByPosibilities('EXPERIMENTS.FILTER_DIALOG.SORT_BY.OUTPUT_TYPE', 'OUTPUT_TYPE',
+    {ascending: 'fa-plug', descending: 'fa-plug'},
     (a: Experiment, b: Experiment) => outputTypeToRaw(a.usedOutputs) - outputTypeToRaw(b.usedOutputs));
-  static readonly OUTPUT_COUNT = new SortByPosibilities('Počtu výstupu', 'OUTPUT_COUNT', {ascending: 'fa-hashtag', descending: 'fa-plug'},
+  static readonly OUTPUT_COUNT = new SortByPosibilities('EXPERIMENTS.FILTER_DIALOG.SORT_BY.OUTPUT_COUNT', 'OUTPUT_COUNT',
+    {ascending: 'fa-hashtag', descending: 'fa-plug'},
     (a: Experiment, b: Experiment) => a.outputCount - b.outputCount);
 
   static readonly VALUES: SortByPosibilities[] = [
@@ -69,8 +74,8 @@ export class SortByPosibilities {
 export class OrderByPosibilities {
   static readonly KEY = 'orderBy';
 
-  static readonly ASCENDING = new OrderByPosibilities('Vzestupně', 'ascending', 'fa-sort-amount-up');
-  static readonly DESCENDING = new OrderByPosibilities('Sestupně', 'descending', 'fa-sort-amount-down-alt');
+  static readonly ASCENDING = new OrderByPosibilities('EXPERIMENTS.FILTER_DIALOG.ORDER_BY.ASCENDING', 'ascending', 'fa-sort-amount-up');
+  static readonly DESCENDING = new OrderByPosibilities('EXPERIMENTS.FILTER_DIALOG.ORDER_BY.DESCENDING', 'descending', 'fa-sort-amount-down-alt');
 
   static readonly VALUES: OrderByPosibilities[] = [
     OrderByPosibilities.ASCENDING,
