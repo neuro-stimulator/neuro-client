33b3e4c787fe3563b8875dbce7126ca1503f1574
petr <stechy12@gmail.com>
2020-01-19 16:08:28 +0100

    Přidání překladu pro addon tlačítka u prohlížeče experimentů v navigační liště.

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ExperimentsRoutingModule } from '../experiments/experiments-routing.module';
-
 import { ContentTogglerDirective } from './content-toggler.directive';
 import { ExperimentTypeResolverDirective } from './experiment-type-resolver.directive';
 import { ExperimentsButtonsAddonComponent } from './buttons-addons/experiments-buttons-addon/experiments-buttons-addon.component';
 import { DateTimePipe } from './date-time.pipe';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -17,6 +16,8 @@ import { DateTimePipe } from './date-time.pipe';
   ],
   imports: [
     CommonModule,
+
+    TranslateModule.forChild()
   ],
   exports: [
     ContentTogglerDirective,
