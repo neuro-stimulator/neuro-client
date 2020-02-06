2938709ae0601c7d32e78017add6075a2033c8ac
petr <stechy12@gmail.com>
2020-02-06 08:49:51 +0100

    Přidáno oznámení, kdy na stimulátor příjde neznámý příkaz.

--- src/app/share/serial.service.ts
+++ src/app/share/serial.service.ts
@@ -100,6 +100,9 @@ export class SerialService {
       case 0x11:
         text = 'Nastavení jednoho výstupu bylo nahráno.';
         break;
+      case 0xF9:
+        text = 'Na stimulátor přišel neznámý příkaz!';
+        break;
     }
 
     this.console.saveCommand({date: new Date(), text});
