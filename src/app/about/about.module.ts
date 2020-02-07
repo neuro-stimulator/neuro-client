d55e006b4fb0f0ee17039e03e1c5fe30153cec47
petr <stechy12@gmail.com>
2020-02-07 10:42:47 +0100

    Přidání jednoduchého popisku aplikace.

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     AboutComponent
   ],
   imports: [
-    AboutRoutingModule
+    AboutRoutingModule,
+    TranslateModule
   ]
 })
 export class AboutModule {
