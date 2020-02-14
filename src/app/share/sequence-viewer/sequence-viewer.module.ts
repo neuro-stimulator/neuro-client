3ca5ba6bfbea66959cee0acb9f9d873465f804f7
petr <stechy12@gmail.com>
2020-02-14 17:25:52 +0100

    Přidání překladů pro prohlížeč sekvencí.

--- src/app/share/sequence-viewer/sequence-viewer.module.ts
+++ src/app/share/sequence-viewer/sequence-viewer.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { SequenceViewerComponent } from './sequence-viewer.component';
 import { CommonModule } from '@angular/common';
 import { ChartsModule } from 'ng2-charts';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     SequenceViewerComponent
   ],
-  imports: [
-    CommonModule,
-    ChartsModule
-  ],
+    imports: [
+        CommonModule,
+        ChartsModule,
+        TranslateModule
+    ],
   exports: [
     SequenceViewerComponent
   ]
