4cba192dc392cf896445a6f55f1ab00ecc11dbee
petr <stechy12@gmail.com>
2020-02-14 16:43:01 +0100

    Přidání překladů pro tag editor + nějaké zapomenuté překlady v halvičce experimentů.

--- src/app/share/tag-editor/tag-editor.module.ts
+++ src/app/share/tag-editor/tag-editor.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { TagEditorComponent } from './tag-editor.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     TagEditorComponent
   ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        TranslateModule
+    ],
   exports: [
     TagEditorComponent
   ]
