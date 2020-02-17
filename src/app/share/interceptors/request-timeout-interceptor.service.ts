a870770f0072fe5f6ad53694463c3fea792f044c
petr <stechy12@gmail.com>
2020-02-17 17:05:22 +0100

    Přeložení hlášky s request timeoutem.

--- src/app/share/interceptors/request-timeout-interceptor.service.ts
+++ src/app/share/interceptors/request-timeout-interceptor.service.ts
@@ -5,15 +5,24 @@ import { Observable, throwError, TimeoutError } from 'rxjs';
 import { catchError, timeout } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 import { NGXLogger } from 'ngx-logger';
+import { TranslateService } from '@ngx-translate/core';
 
 export const DEFAULT_TIMEOUT = new InjectionToken<number>('defaultTimeout');
 
 @Injectable()
 export class RequestTimeoutInterceptor implements HttpInterceptor {
 
+  private timeoutMessage: string;
+
   constructor(@Inject(DEFAULT_TIMEOUT) protected defaultTimeout: number,
               private readonly _toaster: ToastrService,
+              translator: TranslateService,
               private readonly logger: NGXLogger) {
+    translator.get('SHARE.INTERCEPTORS.TIMEOUT')
+              .toPromise()
+              .then((value: string) => {
+                this.timeoutMessage = value;
+              });
   }
 
   /**
@@ -31,7 +40,7 @@ export class RequestTimeoutInterceptor implements HttpInterceptor {
                  catchError((error: any) => {
                    if (error instanceof TimeoutError) {
                      this.logger.warn(`Vypršel timeout pro požadavek: ${req.urlWithParams}`);
-                     this._toaster.warning('Vypršel timeout pro požadavek.');
+                     this._toaster.warning(this.timeoutMessage);
                    }
                    return throwError(error);
                  })
